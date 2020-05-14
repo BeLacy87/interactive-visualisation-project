1243b616b9e23eb1adeefc7e0034c17b618fe82e
BeLacy87 <belacy87@gmail.com>
2020-05-14 14:07:48 -0400

    functionality for bar, bubble, gauge, metadata

--- static/js/app.js
+++ static/js/app.js
@@ -38,10 +38,12 @@ function getData (){
       return i;
     });
     sortedSampleValues.sort((a,b)=>{
-      return b-a;
+      return b-a;    
     })
-    //sliced sample values for bar graph
-    var slicedSampleValues = sortedSampleValues.slice(0,3);
+    
+    //sliced sample values for bar graph, reverse so graph is desc
+    var slicedSampleValues = sortedSampleValues.slice(0,3).reverse();
+    var slicedOtu_ids = otu_ids.slice(0,3).reverse();
 
     console.log(names);    
     console.log(`otu_ids: ${otu_ids}`);   
@@ -53,10 +55,10 @@ function getData (){
     console.log(metadata);  
     console.log (`otu_labels: ${otu_labels}`);
     
-
+//bar chart
     var trace1={
       x: slicedSampleValues,
-      y: otu_ids,
+      y: slicedOtu_ids,
       type: "bar",
       orientation: "h"
     };
@@ -67,134 +69,63 @@ function getData (){
 
   Plotly.newPlot("plot", data, layout);
 
+// //metadata panel
+var panel = d3.select("#sample-metadata");
+var table = panel.append("table");
+var tbody = table.append("tbody");
+Object.keys(metadata).forEach(function(key) {
+    d3.select("tbody");
+    var row = tbody.append("tr");
+    var cell = row.append("td");
+    cell.text(`${key}: `);
+    var cell = row.append("td");
+    cell.text(metadata[key]);
+});
+
+// //gauge plot  
+  var data = [
+    {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: metadata.wfreq,
+      title: { text: 'Belly Button Washing Frequency<br>Scrubs per week' },
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: { axis: { range: [null, 9] }}
+    }
+  ];
+  
+  var layout = { 
+    width: 500, 
+    height: 400,
+    margin: { t: 0, b: 0 } 
+    };
+  Plotly.newPlot('gauge', data, layout);
+
+
+//bubble plot
+var trace2 = {
+  x: otu_idValues,
+  y: sample_values,
+  text: otu_labels,
+  mode: 'markers',
+  marker: {
+    size: sample_values,
+    sizemode: 'area',
+    color: otu_idValues
+  }
+};
+
+var data = [trace2];
+
+var layout = {
+  title: 'Sample values of each OTU',
+  xaxis:{title: "OTU ID"},
+  showlegend: false,
+  height: 600,
+  width: 1150
+};
+
+Plotly.newPlot('bubble', data, layout);
 
 })};
   getData(); 
-  
-
-
-
-
-
-// //metadata panel
-
-
-
-
-
-// var panel = d3.select("#sample-metadata");
-// var table = panel.append("table");
-// var tbody = table.append("tbody");
-
-// metadata.forEach(val=>{
-//   console.log(val); 
-// })
-
-
-
- 
-
-
-
-
-
-
-
-
-  
-  //gauge plot
-  
-
-  // var data = [
-  //   {
-  //     domain: { x: [0, 1], y: [0, 1] },
-  //     value: 270,
-  //     title: { text: "Speed" },
-  //     type: "indicator",
-  //     mode: "gauge+number"
-  //   }
-  // ];
-  
-  // var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-  // Plotly.newPlot('gauge', data, layout);
-
-
-
-
-
-
-
-
-
-
-
-
-  // //bubble plot
-
-
-
-
-  var trace2 = {
-    x: otu_idValues,
-    y: sample_values,
-    text: otu_labels,
-    mode: 'markers',
-    marker: {
-      size: sample_values,
-      sizemode: 'area',
-      color: sample_values
-    }
-  };
-  
-  // var trace2 = {
-  //   x: [1, 2, 3, 4],
-  //   y: [14, 15, 16, 17],
-  //   text: ['A</br>size: 40</br>sixeref: 0.2', 'B</br>size: 60</br>sixeref: 0.2', 'C</br>size: 80</br>sixeref: 0.2', 'D</br>size: 100</br>sixeref: 0.2'],
-  //   mode: 'markers',
-  //   marker: {
-  //     size: [400, 600, 800, 1000],
-  //     //setting 'sizeref' to lower than 1 decreases the rendered size
-  //     sizeref: 2,
-  //     sizemode: 'area'
-  //   }
-  // };
-  
-  // var trace3 = {
-  //   x: [1, 2, 3, 4],
-  //   y: [20, 21, 22, 23],
-  //   text: ['A</br>size: 40</br>sixeref: 2', 'B</br>size: 60</br>sixeref: 2', 'C</br>size: 80</br>sixeref: 2', 'D</br>size: 100</br>sixeref: 2'],
-  //   mode: 'markers',
-  //   marker: {
-  //     size: [400, 600, 800, 1000],
-  //     //setting 'sizeref' to less than 1, increases the rendered marker sizes
-  //     sizeref: 0.2,
-  //     sizemode: 'area'
-  //   }
-  // };
-  
-  // sizeref using above forumla
-  // var desired_maximum_marker_size = 40;
-  // var size = [400, 600, 800, 1000];
-  // var trace4 = {
-  //   x: [1, 2, 3, 4],
-  //   y: [26, 27, 28, 29],
-  //   text: ['A</br>size: 40</br>sixeref: 1.25', 'B</br>size: 60</br>sixeref: 1.25', 'C</br>size: 80</br>sixeref: 1.25', 'D</br>size: 100</br>sixeref: 1.25'],
-  //   mode: 'markers',
-  //   marker: {
-  //     size: size,
-  //     //set 'sizeref' to an 'ideal' size given by the formula sizeref = 2. * max(array_of_size_values) / (desired_maximum_marker_size ** 2)
-  //     sizeref: 2.0 * Math.max(...size) / (desired_maximum_marker_size**2),
-  //     sizemode: 'area'
-  //   }
-  // };
-  
-  var data = [trace2];
-  
-  var layout = {
-    title: 'Bubble Chart Size Scaling',
-    showlegend: false,
-    height: 600,
-    width: 600
-  };
-  
-  Plotly.newPlot('bubble', data, layout);
